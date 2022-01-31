732a7fe2ad8608f5a578e80af9088a144a766934
vjaxel34 <92274553+vjaxel34@users.noreply.github.com>
2022-01-30 19:06:12 -0500

    Hello

---
+++ jsVeryHard/veryHard.js
@@ -0,0 +1,22 @@
+//*VERY HARD: You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+// Hint:  Sudo code this problem. Focus on breaking the problem down into steps  Use functions, arrays and logical operators.  Do not have anyone give you the answer or solve this problem for you. 
+
+// Example 1:
+
+// Input: coins = [1, 2, 5], amount = 11
+
+// Output: 3 
+
+// Explanation: 11 = 5 + 5 + 1
+
+// Example 2:
+
+// Input: coins = [2], amount = 3
+
+// Output: -1
+
+ 
+
+// Note: You may assume that you have an infinite number of each kind of coin.
+
